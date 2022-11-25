c6b68985809681e322fd24a7bac9801aa521ad55
LaloLalo1999 <eduardo.lalo1999@gmail.com>
2022-11-25 10:54:53 -0600

    nuevo main

--- main.c
+++ main.c
@@ -2,28 +2,30 @@
 #include "List.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int menu(List historico);
 
+void abrir_archivo(char *);
+
 int main() {
-    setbuf(stdin, NULL);
-    setbuf(stdout, NULL);
-    char nombre_archivo[50];
+//     setbuf(stdin, NULL);
+//     setbuf(stdout, NULL);
+    char nombre_archivo[20];
     printf("Bienvenido al programa de archivos de texto\n");
-    List historico = listCreate()
+    List historico = listCreate(sizeof(char[20]));
     int opcion;
     do {
         opcion = menu(historico);
         switch (opcion) {
-            case 1:// 
+            case 1:// Abrir archivo
                 printf("Nombre del archivo: ");
                 scanf("%s", nombre_archivo);
                 getchar();
-                Archivo a1 = nuevo_archivo(nombre_archivo);
-                agregar_archivo(historico, a1);
-                abrir_archivo(a1);
+                listAdd(historico, nombre_archivo);
+                abrir_archivo(nombre_archivo);
                 break;
-            case 2://Cerrar el programa
+            case 2:// Cerrar el programa
                 printf("Gracias por utilizar el programa");
                 break;
             case 3:
@@ -46,8 +48,6 @@ int main() {
 
 int menu(List historico) {
     int opcion;
-
-    int opcion;
     unsigned int cant_archivos = listSize(historico);
     printf("\nElige una opcion: \n");
     printf("[1] Abrir archivo\n");
@@ -64,4 +64,32 @@ int menu(List historico) {
     scanf("%d", &opcion);
     getchar();
     return opcion;
+}
+
+void abrir_archivo(char nombre_archivo[20]) {
+    FILE *archivo;
+    archivo = fopen(nombre_archivo, "r");
+    char letra;
+    if (archivo == NULL) {
+        printf("Archivo creado\n");
+        printf("Ingrese el texto: ");
+        char texto[100];
+        scanf("%s", texto);
+        getchar();
+        fclose(archivo);
+        archivo = fopen(nombre_archivo, "w");
+        while (texto != NULL) {
+            letra = texto[0];
+            fputc(letra, archivo);
+            texto++;
+        }
+    } else {
+        printf("Archivo abierto");
+        while(1){
+            letra=fgetc(archivo);
+            if (feof(archivo)) break;
+            printf("%c",letra);
+        }
+    }
+    fclose(archivo);
 }
