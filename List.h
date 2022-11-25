1d0faad12de7fd4fd009acc6be7bb409c91d7232
LaloLalo1999 <eduardo.lalo1999@gmail.com>
2022-11-25 02:01:50 +0000

    Completo rediseño
    Co-authored-by: FAOG99 <FAOG99@users.noreply.github.com>

---
+++ List.h
@@ -0,0 +1,52 @@
+/*
+ * List.h
+ *
+ *  Created on: 1 nov. 2022
+ *      Author: FCERVANTES
+ */
+
+#include <stddef.h>
+
+#ifndef LIST_H_
+#define LIST_H_
+
+typedef void * Type;
+
+typedef struct strList *List;
+
+typedef struct strNode *Node;
+
+List listCreate(size_t);
+
+unsigned int listSize(List);
+
+void listAdd(List, Type);
+
+Type listGet(List, int);
+
+Type listRemove(List, int);
+
+#endif /* LIST_H_ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
